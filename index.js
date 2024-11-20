f6da24e5478f4bf3d4c1e483345814313218e3fa
luizarthurpereirasaraiva <luizarthurpereirasaraiva@gmail.com>
2024-11-20 18:37:24 -0300

    finalizadoParaAvaliar

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// ## Objetivo
-// Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
-// depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-// Se XP for menor do que 1.000 = Ferro
-// Se XP for entre 1.001 e 2.000 = Bronze
-// Se XP for entre 2.001 e 5.000 = Prata
-// Se XP for entre 5.001 e 7.000 = Ouro
-// Se XP for entre 7.001 e 8.000 = Platina
-// Se XP for entre 8.001 e 9.000 = Ascendente
-// Se XP for entre 9.001 e 10.000= Imortal
-// Se XP for maior ou igual a 10.001 = Radiante
-// ## Saída
-// Ao final deve se exibir uma mensagem:
-// "O Herói de nome **{nome}** está no nível de **{nivel}**"
+// A ideia foi criar um ranking a partir da classe de herói/heroína. Com a atribuição de experiência variando 
+// entre as classes, ficou notória a preferência/predileção por algumas classes em detrimento de outras.
 
 let classeRPG = ["escudeiro/a", "cavaleiro/amazona", "mago/a", "ladino/a", "arqueiro/a", "clérico/a", "necromante", "paladino/a"]
 let EXP = [900, 1500, 2800, 5400, 7300, 8500, 9700, 11000]  
