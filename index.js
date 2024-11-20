53ec8d7d5800195479edc05b026759930717036f
luizarthurpereirasaraiva <luizarthurpereirasaraiva@gmail.com>
2024-11-20 18:30:23 -0300

    finalizadoParaAvaliar

--- index.js
+++ index.js
@@ -13,27 +13,29 @@
 // Ao final deve se exibir uma mensagem:
 // "O Herói de nome **{nome}** está no nível de **{nivel}**"
 
-let heroiNome = "Balian"
-let EXP = 1500
-let ranking = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
-if (EXP < 1000){
-    console.log("O herói de nome "+ heroiNome +" está no nível de "+ ranking[0])
+let classeRPG = ["escudeiro", "cavaleiro", "mago", "ladino", "arqueiro", "clérico", "necromante", "paladino"]
+let EXP = [900, 1500, 2800, 5400, 7300, 8500, 9700, 11000]  
+for (let i = 0; i < classeRPG.length; i++){
+    let nome = classeRPG[i]
+    let xp = EXP[i]
+    let ranking;
+
+if (xp < 1000){
+    ranking = "Ferro";
+} else if (xp >= 1001 && xp <= 2000){
+    ranking = "Bronze";
+} else if (xp >= 2001 && xp <= 5000){
+    ranking = "Prata";
+} else if (xp >= 5001 && xp <= 7000){
+    ranking = "Ouro";
+} else if (xp >= 7001 && xp <= 8000){
+    ranking = "Platina";
+} else if (xp >= 8001 && xp <= 9000){
+    ranking = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000){
+    ranking = "Imortal";
+} else if (xp >= 10001){
+    ranking = "Radiante";
 }
-if (1001 < EXP < 2000){
-    console.log("O herói de nome "+ heroiNome +" está no nível de "+ ranking[1])
-}
-if (EXP < 1000){
-    console.log("O herói de nome "+ heroiNome +" está no nível de "+ ranking[2])
-}
-if (EXP < 1000){
-    console.log("O herói de nome "+ heroiNome +" está no nível de "+ ranking[3])
-}
-if (EXP < 1000){
-    console.log("O herói de nome "+ heroiNome +" está no nível de "+ ranking[4])
-}
-if (EXP < 1000){
-    console.log("O herói de nome "+ heroiNome +" está no nível de "+ ranking[5])
-}
-if (EXP < 1000){
-    console.log("O herói de nome "+ heroiNome +" está no nível de "+ ranking[6])
+console.log("O herói da classe "+ nome +" tem "+ xp+" pontos de experiência e está no nível de "+ ranking+".")
 }
